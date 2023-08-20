68c8b11a63bd74e8ed6ec967b5af66f1596e443f
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2023-08-20 14:57:40 +0100

    Update Commit - 08/20/2023 - Update Files 01.

--- config/dotenv.php
+++ config/dotenv.php
@@ -2,17 +2,30 @@
 
 use Dotenv\Dotenv,
 
-    Dotenv\Exception\InvalidPathException;
+    Dotenv\Exception\InvalidPathException, 
+    Dotenv\Exception\ValidationException;
 
 try {
 
-    $dEnv = Dotenv::createImmutable(__DIR__ . '/../');
+    $strEnv = '.env.run'; 
 
+    $dEnv = Dotenv::createImmutable(__DIR__ . '/../tmp/', $strEnv);
+    
     $dEnv -> load();
-
+    
+    $dEnv -> required('APP_ENV') -> notEmpty();
+    $dEnv -> required([ 'APP_NAME', 'APP_DEBUG' ]);
+    $dEnv -> required('TEMPLATE_PROVIDER') -> 
+        allowedValues([ 'twig', 'php-view' ]);
+    
 } 
 catch (InvalidPathException $ipe) {
 
-    echo 'Error load .env';
+    echo $ipe -> getMessage();
+
+}
+catch (ValidationException $ve) {
+
+    echo $ve -> getMessage();
 
 }
